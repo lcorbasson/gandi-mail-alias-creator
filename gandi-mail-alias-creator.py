57a82114706079246af091a7ba192d9b8f2789b6
Loïc CORBASSON <loic.corbasson@gmail.com>
2016-02-05 23:37:34 +0100

    Fix python coding style with autopep8, and fix json indenting for better legibility.

--- gandi-mail-alias-creator.py
+++ gandi-mail-alias-creator.py
@@ -6,36 +6,35 @@ import xmlrpclib
 import json
 
 with open('settings.json', 'r') as settings_file:
-        settings=json.load(settings_file);
+        settings = json.load(settings_file)
 
-mailbox_default=settings['mailbox_default'];
-domain_default=settings['domain_default'];
-apikey_prod=settings['apikey_prod'];
+mailbox_default = settings['mailbox_default']
+domain_default = settings['domain_default']
+apikey_prod = settings['apikey_prod']
 
 if len(sys.argv) < 2:
-        print("Usage: ",sys.argv[0]," alias mailbox(default = ",mailbox_default,") domain(default = ",domain_default,")");
-        exit();
+        print("Usage: ", sys.argv[0], " alias mailbox(default = ", mailbox_default, ") domain(default = ", domain_default, ")")
+        exit()
 
 if len(sys.argv) == 2:
-        mailbox=mailbox_default;
-        domain=domain_default;
+        mailbox = mailbox_default
+        domain = domain_default
 elif len(sys.argv) == 3:
-        mailbox=sys.argv[2];
-        domain=domain_default;
+        mailbox = sys.argv[2]
+        domain = domain_default
 else:
-        mailbox=sys.argv[2];
-        domain=sys.argv[3];
+        mailbox = sys.argv[2]
+        domain = sys.argv[3]
 
-alias=sys.argv[1];
+alias = sys.argv[1]
 
-api=xmlrpclib.ServerProxy("https://rpc.gandi.net/xmlrpc/");
+api = xmlrpclib.ServerProxy("https://rpc.gandi.net/xmlrpc/")
 
-aliases=api.domain.mailbox.info(apikey_prod,domain,mailbox)['aliases'];
+aliases = api.domain.mailbox.info(apikey_prod, domain, mailbox)['aliases']
 if alias in aliases:
-        print('Alias already present',file=sys.stderr);
-        exit();
+        print('Alias already present', file=sys.stderr)
+        exit()
 
-aliases+=[alias];
-aliases.sort();
-api.domain.mailbox.alias.set(apikey_prod,domain,mailbox,aliases);
-
+aliases += [alias]
+aliases.sort()
+api.domain.mailbox.alias.set(apikey_prod, domain, mailbox, aliases)
