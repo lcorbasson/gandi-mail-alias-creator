c74fb3e01469b54f283faa48c13f925917ff1846
Loïc CORBASSON <loic.corbasson@gmail.com>
2016-02-05 23:08:29 +0100

    Add a settings file and rename 'login' to more readable 'mailbox'

--- gandi-mail-alias-creator.py
+++ gandi-mail-alias-creator.py
@@ -2,35 +2,39 @@
 
 import sys
 import xmlrpclib
+import json
 
-login_default='';
-domain_default='';
-apikey_prod='';
+with open('settings.json', 'r') as settings_file:
+        settings=json.load(settings_file);
+
+mailbox_default=settings['mailbox_default'];
+domain_default=settings['domain_default'];
+apikey_prod=settings['apikey_prod'];
 
 if len(sys.argv) < 2:
         print "Usage: %s alias login(default = %s) domain(default = %s)" % (sys.argv[0],login_default,domain_default);
         exit();
 
 if len(sys.argv) == 2:
-        login=login_default;
+        mailbox=mailbox_default;
         domain=domain_default;
 elif len(sys.argv) == 3:
-        login=sys.argv[2];
+        mailbox=sys.argv[2];
         domain=domain_default;
 else:
-        login=sys.argv[2];
+        mailbox=sys.argv[2];
         domain=sys.argv[3];
 
 alias=sys.argv[1];
 
 api=xmlrpclib.ServerProxy("https://rpc.gandi.net/xmlrpc/");
 
-aliases=api.domain.mailbox.info(apikey_prod,domain,login)['aliases'];
+aliases=api.domain.mailbox.info(apikey_prod,domain,mailbox)['aliases'];
 if alias in aliases:
         print 'Alias already present';
         exit();
 
 aliases+=[alias];
 aliases.sort();
-api.domain.mailbox.alias.set(apikey_prod,domain,login,aliases);
+api.domain.mailbox.alias.set(apikey_prod,domain,mailbox,aliases);
 
