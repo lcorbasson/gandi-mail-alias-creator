cf217124de47bbaee3b4fe46fbafbb7a9b53900b
Loïc CORBASSON <loic.corbasson@gmail.com>
2016-02-06 02:09:36 +0100

    Retab, and enhance the TODO

--- gandi-mail-forward-add.py
+++ gandi-mail-forward-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # TODO:
 # - check for each destination if it already exists in the list
+# - make sure calls to api.domain.forward.update are done properly
 
 from __future__ import print_function
 import sys
@@ -29,7 +30,7 @@ else:
         domain = sys.argv[3]
 
 if not isinstance(dest, list):
-	dest = [dest]
+        dest = [dest]
 
 redir = sys.argv[1]
 
@@ -38,7 +39,7 @@ api = xmlrpclib.ServerProxy("https://rpc.gandi.net/xmlrpc/")
 new_redir = False
 destinations = api.domain.forward.list(apikey_prod, domain, {'source': redir})
 if len(destinations) == 0:
-	new_redir = True
+        new_redir = True
 if dest in destinations:
         print('Destination already present', file=sys.stderr)
         exit()
@@ -46,6 +47,6 @@ if dest in destinations:
 destinations += dest
 destinations.sort()
 if new_redir:
-	api.domain.forward.create(apikey_prod, domain, redir, {'destinations': destinations})
-else:	
-	api.domain.forward.update(apikey_prod, domain, redir, {'destinations': destinations})
+        api.domain.forward.create(apikey_prod, domain, redir, {'destinations': destinations})
+else:   
+        api.domain.forward.update(apikey_prod, domain, redir, {'destinations': destinations})
